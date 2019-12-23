ae53201ae04c01715bc3991ae9cc5287fec31165
TekWize.ly <TekWize.ly@gmail.com>
2019-12-22 16:31:29 -0800

    Updates run to v0.6.3

--- Formula/run.rb
+++ Formula/run.rb
@@ -1,8 +1,8 @@
 class Run < Formula
   desc "Easily manage and invoke small scripts and wrappers"
   homepage "https://github.com/TekWizely"
-  url "https://github.com/TekWizely/run/archive/v0.6.2.tar.gz"
-  sha256 "69509b9c7687a765a73555037684faf8c37763fa24e4f7e6a66228df8996271d"
+  url "https://github.com/TekWizely/run/archive/v0.6.3.tar.gz"
+  sha256 "00725eae31e70fb91d708990643303f2a1eb512d116aaea3f586485f854c0358"
 
   depends_on "go" => :build
 
