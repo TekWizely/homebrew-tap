1f85b30010da4c0850e727a6c073b7d71bd4da37
TekWize.ly <TekWize.ly@gmail.com>
2019-12-26 19:49:00 -0800

    Updates homepage; Updates gitSummary on build target

--- Formula/run.rb
+++ Formula/run.rb
@@ -1,13 +1,13 @@
 class Run < Formula
   desc "Easily manage and invoke small scripts and wrappers"
-  homepage "https://github.com/TekWizely"
+  homepage "https://github.com/TekWizely/run"
   url "https://github.com/TekWizely/run/archive/v0.6.5.tar.gz"
   sha256 "5878541e8e29026bf352f78283424217dbf7322a8282cac13b87aa04644e1b58"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-trimpath", "-ldflags", "-w -s -X main.GitSummary=brew", "-o", bin/name
+    system "go", "build", "-trimpath", "-ldflags", "-w -s -X main.GitSummary=brew via tekwizely/tap/run", "-o", bin/name
   end
 
   test do
