0f1c72f7e1c69d426cacdf06a64f8ab9a9736301
TekWize.ly <TekWize.ly@gmail.com>
2020-03-13 16:52:37 -0700

    Update bingo to v0.2.0

--- Formula/bingo.rb
+++ Formula/bingo.rb
@@ -1,8 +1,8 @@
 class Bingo < Formula
   desc "The missing package manager for golang binaries"
   homepage "https://github.com/TekWizely/bingo"
-  url "https://github.com/TekWizely/bingo/archive/v0.1.0.tar.gz"
-  sha256 "8f8e75de0c56b44a89342b4b2adcbdad8c3afe106be26cc1a48fbbb4939a5642"
+  url "https://github.com/TekWizely/bingo/archive/v0.2.0.tar.gz"
+  sha256 "ddad5148d2b2db072658367d2943affc1bbd41095629a21adff0d20f64ce6711"
 
   depends_on "run"
 
@@ -11,7 +11,7 @@ class Bingo < Formula
   end
 
   test do
-    text = "v0.1.0"
+    text = "v0.2.0"
     task = "version"
     assert_equal text, shell_output("#{bin}/#{name} #{task}").chomp
   end
